791f29c403e761fe7c7dfc463fe7850c2467fa59
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-14 10:41:30 +0900

    InsertionSort for문 사용 - 변경사항 추가

--- src/likeLion/week4/day1014/InsertionSort02.java
+++ src/likeLion/week4/day1014/InsertionSort02.java
@@ -9,13 +9,17 @@ public class InsertionSort02 {
         // i = 1 arr[1] j vs arr[0]
         // i = 2 일 때 arr[2] j vs arr[1]
         // i = 2 일 때 arr[1] j vs arr[0]
-        int j= i;
-        if(arr[j] < arr[j-1]) {
-            // indext의 값을 비교 후 자리를 바꾼다.
-            // {8, 5, 6, 2, 4} -> {5, 8, 6, 2, 4}
-            int temp = arr[j]; // int타입의 temp 변수 선언과 동시에 arr[i] 값 temp에 대입한다.
-            arr[j] = arr[j-1]; // arr[i-1]의 값을 arr[i]에 대입한다.
-            arr[j-1] = temp; // temp의 값을 arr[i-1]에 대입한다.
+
+        // loop
+        for(int j = i; j > 0; j--) {
+            System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
+//            if(arr[j] < arr[j-1]) {
+//                // indext의 값을 비교 후 자리를 바꾼다.
+//                // {8, 5, 6, 2, 4} -> {5, 8, 6, 2, 4}
+//                int temp = arr[j]; // int타입의 temp 변수 선언과 동시에 arr[i] 값 temp에 대입한다.
+//                arr[j] = arr[j-1]; // arr[i-1]의 값을 arr[i]에 대입한다.
+//                arr[j-1] = temp; // temp의 값을 arr[i-1]에 대입한다.
+//            }
         }
 
         return arr;
@@ -24,9 +28,8 @@ public class InsertionSort02 {
         // 5에서 출발 합니다.
         int[] arr = {8, 5, 6, 2, 4}; // arr 배열 초기화
 
-        int i = 1; // int i = 1 에서 출발
-
         InsertionSort02 is = new InsertionSort02();
+        arr = new int[] {5, 8, 6, 2, 4};
         int[] result = is.sort(arr, 1); // result에 sort 메서드 실행하여 return값 넣어줌
 
         System.out.println(Arrays.toString(result));
