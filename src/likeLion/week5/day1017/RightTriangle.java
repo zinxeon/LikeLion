5da0e01a4562b0f79e7275f9165cff502bef69ba
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-17 09:12:52 +0900

    알고리즘 1. 별 찍기(직각 삼각형) 4단

---
+++ src/likeLion/week5/day1017/RightTriangle.java
@@ -0,0 +1,11 @@
+package likeLion.week5.day1017;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        // 별 찍기
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
