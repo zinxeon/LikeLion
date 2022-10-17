7fb8ecb8d46f7b82068e5ffb136b34c431e79cb7
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-17 09:32:26 +0900

    별 찍기 - for 문 사용하기

--- src/likeLion/week5/day1017/RightTriangle.java
+++ src/likeLion/week5/day1017/RightTriangle.java
@@ -2,10 +2,22 @@ package likeLion.week5.day1017;
 
 public class RightTriangle {
     public static void main(String[] args) {
-        // 별 찍기
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        // 별 찍기 (직각 삼각형)
+        /*
+        *           1 = 0 별이 1개
+        * *         1 = 0 별이 2개
+        * * *       1 = 0 별이 3개
+        * * * *     1 = 0 별이 4개
+         */
+
+        // for문을 통해 5보다 작을 때 까지 반복
+        for (int i = 0; i < 5; i++) {
+            // j는 i보다 작을 때까지 반복
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
