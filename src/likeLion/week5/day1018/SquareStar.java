1ee0fa4b9548565df4d70abdbd95af2cc121b19d
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-18 09:19:28 +0900

    알고리즘 1. 별 찍기 (정사각형)

---
+++ src/likeLion/week5/day1018/SquareStar.java
@@ -0,0 +1,18 @@
+package likeLion.week5.day1018;
+
+public class SquareStar {
+    public void printSquare(int n) {
+        for (int i = 0; i < n; i++ ) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        SquareStar squareStar = new SquareStar();
+        squareStar.printSquare(3);
+
+    }
+}
