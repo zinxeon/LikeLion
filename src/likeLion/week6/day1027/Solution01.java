878c2f33dc5b2f3b8ffe3a94bb3d8bb01278728e
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-27 09:34:25 +0900

    알고리즘 1. 완주하지 못한 선수

---
+++ src/likeLion/week6/day1027/Solution01.java
@@ -0,0 +1,18 @@
+package likeLion.week6.day1027;
+
+import java.util.HashMap;
+
+public class Solution01 {
+    //마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+    //completion의 길이는 participant의 길이보다 1 작습니다.
+    //participant = 참여한 선수, completion = 완주한 선수,
+    //참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+    //참가자 중에는 동명이인이 있을 수 있습니다.
+
+    //Participant 배열 Completion 배열을 비교해서 없는 이름 찾기
+
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
